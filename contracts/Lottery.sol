37771fad9cd44efd5cc3d4b65655ca5cd513be63
Dean Kang <pregea@icloud.com>
2022-05-25 15:00:33 +0900

    add USDT

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -8,34 +8,42 @@ import "./VRFv2Consumer.sol";
 
 contract Lottery is Ownable {
     using SafeMath for uint256;
+
     IERC20 USDT;
     VRFv2Consumer consumer;
     mapping(address => uint32[][]) public lotteries;
     mapping(uint256 => address) public entries;
     uint256 public totalLottery;
-    uint32[] winner;
+    uint32[] public winner;
     uint32 numbersInLottery = 5;
+    uint decimal = 10 ** 18;
+    uint256 priceOfLottery = 1;
 
-    //using USDT mumbai : 0xA02f6adc7926efeBBd59Fd43A84f4E0c0c91e832
-    constructor(address _vrfv2Interface, address _daiAdderss) {
+    // process
+    // 1. get Randomness / consumer.requestRandomness()
+    // 2. make Lottery and receive USDT / getLotteries()
+    // 3. make winNumber / pickWinNumber()
+    // 4. pick winners / pickWinners()
+
+    /// @notice making randomness, winners will be handled by backend
+    //_usdtAddress : 0xA02f6adc7926efeBBd59Fd43A84f4E0c0c91e832
+    constructor(address _vrfv2Interface, address _usdtAdderss) {
         consumer = VRFv2Consumer(_vrfv2Interface);
-        USDT = IERC20(_daiAdderss);
+        USDT = IERC20(_usdtAdderss);
         totalLottery = 1;
     }
 
     //if getting Lottery is first time, put address in entreis.
     // entries will use for picking winner.
     function getLotteries(uint32 _amount) public {
-        
-        if(lotteries[msg.sender].length == 0){
-            entries[totalLottery] = msg.sender;
-        }
-
+        if(lotteries[msg.sender].length == 0) entries[totalLottery] = msg.sender;
+        transferFrom(msg.sender, address(this), priceOfLottery);
 
         for(uint32 i=0; i< _amount; i++) {
             uint32[] memory ticket = consumer.makeLotteries(i);
             lotteries[msg.sender].push(ticket);
         }
+
         totalLottery = totalLottery.add(_amount);
     }
 
@@ -50,28 +58,24 @@ contract Lottery is Ownable {
 
     function getRank(uint32[] memory _lottery) public view returns(uint32) {
         uint256 count = 0;
+
         for(uint32 i=0; i < numbersInLottery; i++) {
-
-            if(winner[i] == _lottery[i]){
-                count = count.add(1);
-            }
-
+            if(winner[i] == _lottery[i]) count = count.add(1);
         }
+
         uint32 rank = uint32((count > 0 ) ? 6 - count : 0);
         return rank;
     }
 
     function pickWinners() public onlyOwner {
-        for(uint256 i =1; i <totalLottery; i++) {
 
+        for(uint256 i =1; i <totalLottery; i++) {
             address user = entries[i];
             uint32[][] memory lottery = lotteries[user];
 
             for (uint j =0; j < lottery.length; j++) {
                 uint32 rank = getRank(lottery[j]);
-                if(rank != 0) {
-                    transfer(user, rank);
-                }
+                if(rank != 0) transfer(user, rank);
             }
 
         }
@@ -88,14 +92,26 @@ contract Lottery is Ownable {
     }
 
     function transfer(address _to, uint32 _rank) public {
-        uint amount = prize(_rank);
+        uint256 amount = prize(_rank) * decimal;
         require(USDT.balanceOf(address(this)) >= amount, "balance is not enough to giving prize");
         _transfer(_to, amount);
         
     }
 
+    function transferFrom(address _from, address _to, uint256 _amount) public {
+        _amount = _amount * decimal;
+        require(USDT.balanceOf(_from) > _amount, "Not eonough tokens.");
+        _transferFrom(_from, _to, _amount);
+        
+    }
+
     function _transfer(address _to, uint256 _amount) private {
         (bool success, bytes memory data) = address(USDT).call(abi.encodeWithSelector(bytes4(keccak256("transfer(address,uint)")), _to, _amount));
+        require(success && (data.length == 0 || abi.decode(data, (bool))), "TF");
+    }
+
+    function _transferFrom(address _from, address _to, uint256 _amount) private {
+        (bool success, bytes memory data) = address(USDT).call(abi.encodeWithSelector(bytes4(keccak256("transferFrom(address,address,uint)")),_from, _to, _amount));
         require(success && (data.length == 0 || abi.decode(data, (bool))), "TF");
 
     }
--- contracts/VRFv2Consumer.sol
+++ contracts/VRFv2Consumer.sol
@@ -8,65 +8,61 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract VRFv2Consumer is VRFConsumerBaseV2, Ownable {
 
-  using SafeMath for uint256;
-  VRFCoordinatorV2Interface COORDINATOR;
+    using SafeMath for uint256;
 
-  // Your subscription ID.
-  uint64 s_subscriptionId;
+    VRFCoordinatorV2Interface COORDINATOR;
 
-  // Polygon Mumbai Testnet coordinator.
-  address vrfCoordinator = 0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed;
-  bytes32 keyHash = 0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f;
-  uint32 callbackGasLimit = 100000;
-  uint16 requestConfirmations = 2;
+    uint64 s_subscriptionId;
+    address vrfCoordinator = 0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed;
+    bytes32 keyHash = 0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f;
+    uint32 callbackGasLimit = 100000;
+    uint16 requestConfirmations = 2;
+    address s_owner;
+    uint32 numWords = 1;
+    uint32 numbersInLottery = 5;
+    uint256[] public s_randomWords;
+    uint256 public s_requestId;
 
+    constructor(uint64 subscriptionId) VRFConsumerBaseV2(vrfCoordinator) {
+        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+        s_owner = msg.sender;
+        s_subscriptionId = subscriptionId;
+    }
 
-  uint256[] public s_randomWords;
-  uint256 public s_requestId;
-  address s_owner;
-  uint32 numWords = 1;
-  uint32 numbersInLottery = 5;
-
-
-  constructor(uint64 subscriptionId) VRFConsumerBaseV2(vrfCoordinator) {
-    COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
-    s_owner = msg.sender;
-    s_subscriptionId = subscriptionId;
-  }
-
-  // Assumes the subscription is funded sufficiently.
-  // https://vrf.chain.link/mumbai
-  function requestRandomness() public onlyOwner {
+    // Assumes the subscription is funded sufficiently.
+    // https://vrf.chain.link/mumbai
+    function requestRandomness() public {
     // Will revert if subscription is not set and funded.
-    s_requestId = COORDINATOR.requestRandomWords(
-      keyHash,
-      s_subscriptionId,
-      requestConfirmations,
-      callbackGasLimit,
-      numWords
-    );
+        s_requestId = COORDINATOR.requestRandomWords(
+            keyHash,
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+    }
 
-  }
-
-  function makeLotteries(uint32 _seed) public view returns (uint32[] memory) {
+    function makeLotteries(uint32 _seed) public view returns (uint32[] memory) {
     require(s_randomWords[0] != 0 , "not fulfill yet");
-
     uint256 randomNumber = uint256(keccak256(abi.encode(s_randomWords[0], _seed)));
     uint32[] memory lottery = new uint32[](numbersInLottery);
-    for(uint32 j =0; j < numbersInLottery; j++) {
-      uint32 num = uint32(randomNumber.mod(100));
-      randomNumber = randomNumber.div(100);
-      lottery[j] = num;
-    }
-    return lottery;
-  }
-    
 
-  // fallback for requestRandomness
-  function fulfillRandomWords(
-    uint256, /* requestId */
-    uint256[] memory randomWords
-  ) internal override {
-    s_randomWords = randomWords;
-  }
+    for(uint32 j =0; j < numbersInLottery; j++) {
+        uint32 num = uint32(randomNumber.mod(100));
+        randomNumber = randomNumber.div(100);
+        lottery[j] = num;
+    }
+
+    return lottery;
+    
+    }
+
+    // fallback for requestRandomness
+    function fulfillRandomWords(
+        uint256, /* requestId */
+        uint256[] memory randomWords
+        ) internal override {
+        s_randomWords = randomWords;
+    }
+
 }
